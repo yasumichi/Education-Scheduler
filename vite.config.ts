a59d77a7137fca97f252fef0972daa24e65d3a61
Yasumichi Akahoshi <sekiseijuju@gmail.com>
2026-04-11 19:05:48 +0900

    Add BASE_URL

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 
 export default defineConfig({
+  base: './',
   plugins: [preact()],
   server: {
     host: true, // Listen on all local IP addresses (0.0.0.0)
